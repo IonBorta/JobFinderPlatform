c1030ac1cb6a08f83dc8fb8c999c75d497c7ceda
Ion <ion.borta111@mail.ru>
2024-11-13 00:44:43 +0200

    Add project files.

---
+++ JobFinder.Web/Controllers/JobController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobFinder.Web.Controllers
+{
+    public class JobController : Controller
+    {
+        public IActionResult Jobs()
+        {
+            return View();
+        }
+        public IActionResult JobDetails()
+        {
+            return View();
+        }
+    }
+}
---
+++ JobFinder.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JobFinder.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
